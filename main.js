62cf4f4697919783c7c01d95976261152f97ee6a
Paweł Murias <pawelmurias@gmail.com>
2018-10-02 13:47:01 +0200

    Load .p6 code from gist

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-console.log('starting up 6pad');
 
 const editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     theme: 'zenburn'
@@ -9,3 +8,20 @@ document.getElementById('runbutton').addEventListener('click', function() {
     console.log('Running', code);
     eval(code);
 });
+
+async function loadGist(gist) {
+  const response = await fetch("https://api.github.com/gists/" + gist);
+  const json = await response.json();
+  for (const fileName in json.files) {
+      const file = json.files[fileName];
+      if (/.p6$/.test(fileName)) {
+          editor.setValue(file.content);
+      }
+  }
+  console.log(json.files);
+}
+
+
+if (document.location.hash) {
+    loadGist(document.location.hash.substr(1));
+}
